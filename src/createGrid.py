0d7f92473dbc99764f2ec1302aaa183018c81a33
Petros <petroshatt@gmail.com>
2021-04-30 14:28:47 +0300

    Update with interactive window

--- src/createGrid.py
+++ src/createGrid.py
@@ -2,6 +2,79 @@ import pygame
 import json
 import datetime
 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+
+def pars_json(file):
+    
+  f = open(file)
+  
+  data = json.load(f)
+
+  f.close()
+
+  return data
+
+
+def reloadJson():
+  global json_data 
+  json_data = pars_json("../data/json.json")
+
+
+
+#json_data = pars_json("../data/json.json")
+json_data = pars_json("../data/json.json")
+
+
+# Initialize pygame
+pygame.init()
+ 
+# Set title of screen
+pygame.display.set_caption("Grid")
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+pygame.font.init() # you have to call this at the start, 
+                   # if you want to use this module.
+myfont = pygame.font.SysFont('Arial', 20)
+
+textsurface = myfont.render('r1', False, RED)
+
+screen = pygame.display.set_mode((0,0), 0, 0)
+
+
+
+# This sets the margin between each cell
+MARGIN = 2
+#This sets the default size of each cell on the grid
+GRID_SIZE = json_data["grid_size"]
+
+TILE_SCROLL = 3
+
+#Set the dimensions of the grid
+y_dimension = json_data["ydimension"]+1
+x_dimension = json_data["xdimension"]+1
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = GRID_SIZE
+HEIGHT = GRID_SIZE
+
+
+map_width = GRID_SIZE*x_dimension + MARGIN*x_dimension
+map_height = GRID_SIZE*y_dimension + MARGIN*y_dimension
+main_map = pygame.Surface((map_width, map_height))
+main_map = main_map.convert()
+done = False
+
+
+scroll_x = 0
+scroll_y = 0
+
 def txtOutput():
 
   filename_txt = "stats--" + datetime.datetime.now().strftime("%d-%m-%Y--%H-%M-%S") + ".txt"
@@ -31,60 +104,35 @@ def txtOutput():
   print("Stats saved as: ", filename_txt)
 
 
-def pars_json(file):
-    
-  f = open(file)
-  
-  data = json.load(f)
-
-  f.close()
-
-  return data
+def gridOutput():
+  filename_grid = "grid--" + datetime.datetime.now().strftime("%d-%m-%Y--%H-%M-%S") + ".png"
+  pygame.image.save(main_map, filename_grid)
+  print("\nGrid saved as: " + filename_grid)
 
 
-json_data = pars_json("../data/json.json")
+def initWindow():
+
+  win_x = GRID_SIZE * x_dimension + MARGIN*x_dimension
+  win_y = GRID_SIZE * y_dimension + MARGIN*y_dimension
+
+  DISPLAY_SIZE = (int(win_x), int(win_y))
+  DEPTH = 32
+  FLAGS = 0
+  #screen = pygame.display.set_mode(DISPLAY_SIZE, FLAGS, DEPTH)
+  screen = pygame.display.set_mode((800,800))
 
 
-DISPLAY = True
 
-
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-
-# This sets the margin between each cell
-MARGIN = 1
-#This sets the default size of each cell on the grid
-GRID_SIZE = json_data["grid_size"]
-
-#Set the dimensions of the grid
-y_dimension = json_data["ydimension"]+1
-x_dimension = json_data["xdimension"]+1
-
-if (x_dimension or y_dimension) > 2000:
-  DISPLAY = False
-
-
-win_x = GRID_SIZE * x_dimension + MARGIN*x_dimension
-win_y = GRID_SIZE * y_dimension + MARGIN*y_dimension +GRID_SIZE*2
-
-# This sets the WIDTH and HEIGHT of each grid location
-WIDTH = GRID_SIZE
-HEIGHT = GRID_SIZE
-
-if DISPLAY==True:
-
-  # Create a 2 dimensional array. A two dimensional
-  # array is simply a list of lists.
+def initGrid():
+  global grid
+  #fill grid
   grid = []
   for row in range(y_dimension):
-      # Add an empty array that will hold each cell
-      # in this row
-      grid.append([])
-      for column in range(x_dimension):
-          grid[row].append((255, 255, 255))  # Append a cell
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(x_dimension):
+        grid[row].append((255, 255, 255))  # Append a cell
 
   #INIT AXIS
   for i in range(y_dimension):
@@ -93,11 +141,18 @@ if DISPLAY==True:
   for i in range(x_dimension):
     grid[0][i] = i
 
+  # Set the screen background
+  screen.fill(BLACK)
+
+def init_objects():
+  initObstacles()
+  initRobots()
+
+def initObstacles():
 
   #INIT OBSTACLES
   temp_row_obs = 0
   temp_col_obs = 0
-
 
   num_of_groups = len(json_data["obstacle"])
 
@@ -108,14 +163,15 @@ if DISPLAY==True:
     color = json_data["obstacle"][i]["group_color"]
     color = color.split("(")
     color = color[1].split(")")
-    #print(color[0])
     color1, color2, color3 = color[0].split(",")
 
     for j in range(num_of_obs):
       temp_row_obs = json_data["obstacle"][i]["group_pos"][j][0]
       temp_col_obs = json_data["obstacle"][i]["group_pos"][j][1]
-      #print(type(temp_col_obs))
       grid[temp_row_obs][temp_col_obs] = (int(color1),int(color2),int(color3))
+
+
+def initRobots():
 
 
   #INIT ROBOTS
@@ -130,142 +186,126 @@ if DISPLAY==True:
     grid[temp_row_rob][temp_col_rob] = json_data["robots"][i]["robot_name"]
 
 
+def eventLoop():
+  global done
+  global scroll_x
+  global scroll_y
+  for event in pygame.event.get():  # User did something
+      if event.type == pygame.QUIT:  # If user clicked close
+          done = True
+      elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+          print("up")
+          #scroll_y = scroll_y + (GRID_SIZE*TILE_SCROLL+MARGIN*TILE_SCROLL)
+          scroll_y = min(scroll_y + (GRID_SIZE*TILE_SCROLL+MARGIN*TILE_SCROLL), 0)
+        elif event.key == pygame.K_DOWN:
+          print("down")
+          #scroll_y = scroll_y - (GRID_SIZE*TILE_SCROLL+MARGIN*TILE_SCROLL)
+          scroll_y = max(scroll_y - (GRID_SIZE*TILE_SCROLL+MARGIN*TILE_SCROLL), -(TILE_SCROLL*800))
+        elif event.key == pygame.K_LEFT:
+          print("left")
+          #scroll_x = scroll_x + (GRID_SIZE*TILE_SCROLL+MARGIN*TILE_SCROLL)
+          scroll_x = min(scroll_x + (GRID_SIZE*TILE_SCROLL+MARGIN*TILE_SCROLL), 0)
+        elif event.key == pygame.K_RIGHT:
+          print("right")
+          #scroll_x = scroll_x - (GRID_SIZE*TILE_SCROLL+MARGIN*TILE_SCROLL)
+          scroll_x = max(scroll_x - (GRID_SIZE*TILE_SCROLL+MARGIN*TILE_SCROLL), -(TILE_SCROLL*800))
 
-  # Initialize pygame
-  pygame.init()
+def draw():
 
-  # Set the HEIGHT and WIDTH of the screen
-  WINDOW_SIZE = [int(win_x), int(win_y)]
+  # Draw the grid
+  for row in range(y_dimension):
+    for column in range(x_dimension):
+
+      #case tile is GRID AXIS
+      if type(grid[row][column]) == int:
+        color = (0, 0, 0)
+      #case tile is ROBOT
+      elif type(grid[row][column]) == str:
+        color = (0, 0, 0)
+      #case tile is OBSTACLE
+      else:
+        color = grid[row][column]
+
+      pygame.draw.rect(main_map,
+                       color,
+                       [(MARGIN + WIDTH) * column + MARGIN,
+                        (MARGIN + HEIGHT) * row + MARGIN,
+                         WIDTH,
+                         HEIGHT])
+
+      #text in case tile is GRID AXIS
+      if type(grid[row][column]) == int:
+        textsurface = myfont.render(str(grid[row][column]), False, RED)
+        axis_y = GRID_SIZE*row + MARGIN*row
+        axis_x = GRID_SIZE*column + MARGIN*column +2
+        main_map.blit(textsurface,(axis_x,axis_y))
+
+      #text in case tile is ROBOT
+      if type(grid[row][column]) == str:
+        textsurface = myfont.render(grid[row][column], False, RED)
+        rob_y = GRID_SIZE*row + MARGIN*row
+        rob_x = GRID_SIZE*column + MARGIN*column +2
+        main_map.blit(textsurface,(rob_x,rob_y))  
+
+  # Draw the lines
+  num_of_rob = len(json_data["robots_movements"])
+
+  for k in range(num_of_rob):
+
+    num_of_moves = len(json_data["robots_movements"][k-1]["move"])-1
+    line_color = json_data["robots_movements"][k-1]["line_color"]
+    line_color = line_color.split("(")
+    line_color = line_color[1].split(")")
+    color1, color2, color3 = line_color[0].split(",")
+    line_color = (int(color1),int(color2),int(color3))
+
+    for i in range(num_of_moves):
+      temp_move_y1 = json_data["robots_movements"][k-1]["move"][i][0]
+      temp_move_y2 = json_data["robots_movements"][k-1]["move"][i+1][0]
+      temp_move_x1 = json_data["robots_movements"][k-1]["move"][i][1]
+      temp_move_x2 = json_data["robots_movements"][k-1]["move"][i+1][1]
+
+      move_y1 = GRID_SIZE/2 + (GRID_SIZE*(temp_move_y1)) + MARGIN*temp_move_y1 + k*1
+      move_y2 = GRID_SIZE/2 + (GRID_SIZE*(temp_move_y2)) + MARGIN*temp_move_y2 + k*1
+      move_x1 = GRID_SIZE/2 + (GRID_SIZE*(temp_move_x1)) + MARGIN*temp_move_x1 + k*1
+      move_x2 = GRID_SIZE/2 + (GRID_SIZE*(temp_move_x2)) + MARGIN*temp_move_x2 + k*1
+
+      pygame.draw.line(main_map, line_color, (move_x1,move_y1), (move_x2,move_y2), 3)   
+
+  screen.blit(main_map, (scroll_x, scroll_y))
+  pygame.display.flip()
 
 
-  DISPLAY_SIZE = (1, 1)
-  DEPTH = 32
-  FLAGS = 0
-  screen = pygame.display.set_mode(DISPLAY_SIZE, FLAGS, DEPTH)
+def run():
+  global clock
+  #global json_data
+  while not done:
+    # Limit to 60 frames per second
+    clock.tick()
 
-  work_surface  = pygame.Surface((int(win_x), int(win_y)))
+    eventLoop()
+    reloadJson()
+    initGrid()
+    initObstacles()
+    initRobots()
 
-   
-  # Set title of screen
-  pygame.display.set_caption("Grid")
-   
-  # Loop until the user clicks the close button.
-  done = True
+    draw()
 
-  # Used to manage how fast the screen updates
-  clock = pygame.time.Clock()
-
-  
-  pygame.font.init() # you have to call this at the start, 
-                     # if you want to use this module.
-  myfont = pygame.font.SysFont('Arial', 7)
-
-  textsurface = myfont.render('r1', False, RED)
-  
-
-  saved_counter = 1
 
 
 # -------- Main Program Loop -----------
-while DISPLAY:
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.QUIT:  # If user clicked close
-            DISPLAY = False
- 
-    # Set the screen background
-    work_surface.fill(BLACK)
- 
-    # Draw the grid
-    for row in range(y_dimension):
-        for column in range(x_dimension):
+if __name__ == "__main__":
 
-            #case tile is GRID AXIS
-            if type(grid[row][column]) == int:
-              color = (0, 0, 0)
-            #case tile is ROBOT
-            elif type(grid[row][column]) == str:
-              color = (0, 0, 0)
-            #case tile is OBSTACLE
-            else:
-              color = grid[row][column]
+  reloadJson()
+  initWindow()
+  initGrid()
+  run()
 
-            pygame.draw.rect(work_surface,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                               WIDTH,
-                               HEIGHT])
+  gridOutput()
+  txtOutput()
 
-            #text in case tile is GRID AXIS
-            if type(grid[row][column]) == int:
-              textsurface = myfont.render(str(grid[row][column]), False, RED)
-              axis_y = GRID_SIZE*row + MARGIN*row
-              axis_x = GRID_SIZE*column + MARGIN*column +2
-              work_surface.blit(textsurface,(axis_x,axis_y))
+  pygame.quit()
 
-            #text in case tile is ROBOT
-            if type(grid[row][column]) == str:
-              textsurface = myfont.render(grid[row][column], False, RED)
-              rob_y = GRID_SIZE*row + MARGIN*row
-              rob_x = GRID_SIZE*column + MARGIN*column +2
-              work_surface.blit(textsurface,(rob_x,rob_y))    
+  import heatmap
 
-    # Draw the lines
-    num_of_rob = len(json_data["robots_movements"])
-
-    for k in range(num_of_rob):
-
-      num_of_moves = len(json_data["robots_movements"][k-1]["move"])-1
-      line_color = json_data["robots_movements"][k-1]["line_color"]
-      line_color = line_color.split("(")
-      line_color = line_color[1].split(")")
-      color1, color2, color3 = line_color[0].split(",")
-      line_color = (int(color1),int(color2),int(color3))
-
-      for i in range(num_of_moves):
-        temp_move_y1 = json_data["robots_movements"][k-1]["move"][i][0]
-        temp_move_y2 = json_data["robots_movements"][k-1]["move"][i+1][0]
-        temp_move_x1 = json_data["robots_movements"][k-1]["move"][i][1]
-        temp_move_x2 = json_data["robots_movements"][k-1]["move"][i+1][1]
-
-        move_y1 = GRID_SIZE/2 + (GRID_SIZE*(temp_move_y1)) + MARGIN*temp_move_y1 + k*1
-        move_y2 = GRID_SIZE/2 + (GRID_SIZE*(temp_move_y2)) + MARGIN*temp_move_y2 + k*1
-        move_x1 = GRID_SIZE/2 + (GRID_SIZE*(temp_move_x1)) + MARGIN*temp_move_x1 + k*1
-        move_x2 = GRID_SIZE/2 + (GRID_SIZE*(temp_move_x2)) + MARGIN*temp_move_x2 + k*1
-
-        pygame.draw.line(work_surface, line_color, (move_x1,move_y1), (move_x2,move_y2), 1)
-
-
-    label_x = 0+35
-    label_y = GRID_SIZE*y_dimension+MARGIN*y_dimension+2
-
-    myfont = pygame.font.SysFont('Arial', 20)
-    textsurface = myfont.render(str(json_data["label"]), False, RED)
-    work_surface.blit(textsurface,(label_x,label_y))  
-
-
-    filename_grid = "grid--" + datetime.datetime.now().strftime("%d-%m-%Y--%H-%M-%S") + ".png"
-
-    pygame.image.save(work_surface, filename_grid)
-
-     
-    if saved_counter==1:
-      print("\nGrid saved as: " + filename_grid)
-    saved_counter+=1
-
-    DISPLAY = False
-
-    # Limit to 60 frames per second
-    clock.tick(60)
- 
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-
- 
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
-pygame.quit()
-
-txtOutput()
-
-import heatmap
